0b540498183616ffe8f2780b18fbca3ed973689f
Henik <henrikwendt95@gmail.com>
2023-02-14 17:34:13 +0100

    New implementation of data storage and, new functionallity to show waterings of different nodes.

--- UIServer/src/server/models/Data.js
+++ UIServer/src/server/models/Data.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
-let  dataSchema = new Schema(
-  {
-    name: {
-        type: String,
-        required: true,
-        unique: true
-      },
-    numberOfWaterings: {
-      type: Array,
-    },
+let dataSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
   },
-
-);
+  name: {
+    type: String,
+    required: true,
+  },
+  numberOfWaterings: {
+    type: Number,
+  },
+});
 
 let Data = mongoose.model("data", dataSchema);
 
--- UIServer/src/server/routes/fetchData.js
+++ UIServer/src/server/routes/fetchData.js
@@ -1,146 +1,174 @@
-var express = require('express');
-var request = require('request');
+var express = require("express");
+var request = require("request");
 var User = require("../models/User");
 var Data = require("../models/Data");
 var router = express.Router();
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 
 /* GET home page. */
 
-const dataPassword = "1uy23g124kjbacAWD/&Da2he12!?"
-router.get('/chart', async (req, res) => {
+const dataPassword = "1uy23g124kjbacAWD/&Da2he12!?";
+router.get("/chart", async (req, res) => {
+  try {
+    var parameters = req.query;
 
-    try{
+    const user = await User.findOne({ username: parameters.username }).exec();
+    if (
+      user !== null &&
+      parameters.token !== "null" &&
+      bcrypt.compareSync(user.token, parameters.token)
+    ) {
+      //const data = await Data.findOne({ name: "wateringData" }).exec();
 
-        var parameters = req.query;
-    
-        const user = await User.findOne({username: parameters.username}).exec();
-        console.log(user);
-        if(user !== null && parameters.token !== "null" && bcrypt.compareSync(user.token, parameters.token)) {
+      let data = null;
+      if (parameters.node === "All") {
+        const listOfDates = await calculateDates();
+        let tempData = await Data.find().where("date").in(listOfDates).exec();
 
-            const data = await Data.findOne({name: "wateringData"}).exec();
-            if(data !== null){
+        let mapOfDates = new Map();
 
-            res.status(200).json({
-                status: 200,
-                numberOfWaterings: data.numberOfWaterings,
-                message: "Data was sent!",
-            });
-        }else {
-            res.status(400).json({
-                status: 400,
-                message: "Error, no data was found!",
-            });
-        }
-        
-        }else {
-            res.status(400).json({
-                status: 400,
-                message: "Not authorized!",
-            });
-        }
-    }catch(err) {
-       
-    }
-    
-});
-
-router.get('/status', async (req, res) => {
-
-    try{
-        var parameters = req.query;
-
-        const user = await User.findOne({username: parameters.username}).exec();
-        if(user !== null && parameters.token !== "null" && bcrypt.compareSync(user.token, parameters.token)) {
-
-            const data = await Data.findOne({name: "wateringData"}).exec();
-            if(data !== null){
-
-                res.status(200).json({
-                    status: 200,
-                    wateringStatus: await calculateStatus(data.numberOfWaterings),
-                    message: "Data was sent!",
-                });
-            }else {
-                res.status(400).json({
-                    status: 400,
-                    message: "Error, no data was found!",
-                });
-            }
-        
-        }else {
-            res.status(400).json({
-                status: 400,
-                message: "Not authorized!",
-            });
-        }
-    }catch(err) {
-       
-    }
-
-});
-router.post('/', async (req, res) => {
-
-   try{
-        var parameters = req.query;
-       
-        if(dataPassword === parameters.password) {
-            const data = await Data.findOne({name: "wateringData"}).exec();
-            
-            if(data !== null){
-                data.numberOfWaterings = await fixData(parameters.numberOfWaterings);
-                await data.save();
-            }else {
-                let newData = new Data();
-                newData.name = "wateringData";
-                newData.numberOfWaterings =  await fixData(parameters.numberOfWaterings);
-                newData = await newData.save();
-            }
-            res.status(200).json({
-                status: 200,
-                message: "Data was recived!",
-            });
-    
-        }else{
-            res.status(400).json({
-                status: 400,
-                message: "Wrong password!",
-            });
-        }
-    }catch(err) {
-        res.status(400).json({
-            status: 400,
-            message: "Someting went wrong!",
+        tempData.forEach((element) => {
+          if (mapOfDates.has(element.date.toJSON().slice(0, 10))) {
+            mapOfDates.get(
+              element.date.toJSON().slice(0, 10)
+            ).numberOfWaterings =
+              mapOfDates.get(element.date.toJSON().slice(0, 10))
+                .numberOfWaterings + element.numberOfWaterings;
+          } else {
+            mapOfDates.set(element.date.toJSON().slice(0, 10), element);
+          }
         });
+        let tempList = [];
+        mapOfDates.forEach((element) => {
+          tempList.push(element);
+        });
+        data = tempList;
+      } else {
+        const listOfDates = await calculateDates();
+        data = await Data.find({ name: "node-" + parameters.node })
+          .where("date")
+          .in(listOfDates)
+          .exec();
+      }
+
+      if (data !== null) {
+        res.status(200).json({
+          status: 200,
+          data: data,
+          message: "Data was sent!",
+        });
+      } else {
+        res.status(400).json({
+          status: 400,
+          message: "Error, no data was found!",
+        });
+      }
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "Not authorized!",
+      });
     }
+  } catch (err) {}
 });
 
-async function fixData (data) {
-    let tempArray = data.split('*');
-    let newArray = [];
-    for(var i = 0; i < tempArray.length; i++) {
-        newArray.push(JSON.parse(tempArray[i]));
+router.get("/status", async (req, res) => {
+  try {
+    var parameters = req.query;
+
+    const user = await User.findOne({ username: parameters.username }).exec();
+    if (
+      user !== null &&
+      parameters.token !== "null" &&
+      bcrypt.compareSync(user.token, parameters.token)
+    ) {
+      let currentDate = new Date().toJSON().slice(0, 10);
+      const data = await Data.find().where("date").in(currentDate).exec();
+      if (data !== null) {
+        res.status(200).json({
+          status: 200,
+          wateringStatus: await calculateStatus(data),
+          message: "Data was sent!",
+        });
+      } else {
+        res.status(400).json({
+          status: 400,
+          message: "Error, no data was found!",
+        });
+      }
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "Not authorized!",
+      });
     }
-    return newArray;
+  } catch (err) {}
+});
+router.post("/", async (req, res) => {
+  try {
+    var parameters = req.query;
+
+    if (dataPassword === parameters.password) {
+      const data = await Data.findOne({
+        name: parameters.name,
+        date: parameters.date,
+      }).exec();
+      if (data !== null) {
+        data.numberOfWaterings =
+          parseInt(data.numberOfWaterings) +
+          parseInt(parameters.numberOfWaterings);
+        await data.save();
+      } else {
+        let newData = new Data();
+        newData.name = parameters.name;
+        newData.date = parameters.date;
+        newData.numberOfWaterings = parameters.numberOfWaterings;
+        await newData.save();
+      }
+      res.status(200).json({
+        status: 200,
+        message: "Data was recived!",
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "Wrong password!",
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: "Someting went wrong!",
+    });
+  }
+});
+
+async function calculateDates() {
+  let listOfDates = [];
+  for (let i = 0; i < 7; i++) {
+    const newDate = new Date();
+    newDate.setDate(newDate.getDate() - i);
+    listOfDates.push(newDate.toJSON().slice(0, 10));
+  }
+  return listOfDates;
 }
 
+async function calculateStatus(data) {
+  let sum = 0;
 
-async function calculateStatus (data) {
-    let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum = sum + data[i].numberOfWaterings;
+  }
 
-   for(var i = 0; i < data.length; i++) {
-        sum = sum + data[i].number;
-    }
-
-    if(12 < sum){
-        return " High temperature, a large number of waterings needed. "
-    }else if(6 <= sum <= 12) {
-        return " Moderat temperature, avarage number of waterings needed. "
-    }else if (6 < sum) {
-        return " Low temperature, few waterings needed. "
-    }else {
-        return " Someting went wrong. "
-    }
+  if (12 < sum) {
+    return " High temperature, a large number of waterings needed. ";
+  } else if (6 <= sum <= 12) {
+    return " Moderat temperature, avarage number of waterings needed. ";
+  } else if (6 < sum) {
+    return " Low temperature, few waterings needed. ";
+  } else {
+    return " Someting went wrong. ";
+  }
 }
 
 module.exports = router;
